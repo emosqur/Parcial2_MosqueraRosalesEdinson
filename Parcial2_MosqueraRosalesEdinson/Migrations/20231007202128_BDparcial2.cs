bbc859bf779d3632840a4998562e7713293440c3
edinson rosales <edidy9v@gmail.com>
2023-10-07 15:31:02 -0500

    database is created in the local entity

---
+++ Parcial2_MosqueraRosalesEdinson/Migrations/20231007202128_BDparcial2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial2_MosqueraRosalesEdinson.Migrations
+{
+    public partial class BDparcial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NaturalsPersons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    BirtYear = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifieldDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NaturalsPersons", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NaturalsPersons_Email",
+                table: "NaturalsPersons",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NaturalsPersons_FullName",
+                table: "NaturalsPersons",
+                column: "FullName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NaturalsPersons");
+        }
+    }
+}
