5481074567a3161dcb82a8598b4d8209adc1df09
edinson rosales <edidy9v@gmail.com>
2023-10-07 11:03:20 -0500

    Creation of the project in ASP.NETCore

---
+++ Parcial2_MosqueraRosalesEdinson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial2_MosqueraRosalesEdinson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
