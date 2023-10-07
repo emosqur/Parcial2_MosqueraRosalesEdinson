99e34109becd830b8f6eb92388cab8bd802a8a55
edinson rosales <edidy9v@gmail.com>
2023-10-07 14:12:50 -0500

    we name the database

--- Parcial2_MosqueraRosalesEdinson/DAL/DataBaseContext.cs
+++ Parcial2_MosqueraRosalesEdinson/DAL/DataBaseContext.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_MosqueraRosalesEdinson.DAL.Entities;
 
 namespace Parcial2_MosqueraRosalesEdinson.DAL
 {
     public class DataBaseContext : DbContext
     {
+        public DataBaseContext(DbContextOptions<DataBaseContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<NaturalPerson> NaturalsPersons { get; set; }
+
+
     }
 }
